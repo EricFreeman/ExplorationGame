f0da73b11c70a23c8af508222b04315d5e016f15
Eric Freeman <efreeman@1800contacts.com>
2015-07-28 21:55:11 -0600

    Added keyboard movement to walk around.

---
+++ src/Assets/Scripts/Player/KeyboardMovement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    public float MoveSpeed = 3.5f;
+
+    void Update()
+    {
+        var horizontalMovement = Input.GetAxisRaw("Horizontal");
+        var verticalMovement = Input.GetAxisRaw("Vertical");
+
+        var movement = new Vector3(horizontalMovement, 0, verticalMovement) * Time.deltaTime;
+
+        transform.Translate(movement);
+    }
+}
